84766d7db599ded3b17d2078226d8f21084a6cec
Maxim Sinyakov <m.sinyakov@cian.ru>
2020-02-24 04:37:06 +0300

    wordcount solution

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,28 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+func main() {
+	filenames := os.Args[1:]
+	counts := map[string]int{}
+
+	for i := 0; i < len(filenames); i++ {
+		data, _ := ioutil.ReadFile(filenames[i])
+		lines := strings.Split(string(data), "\n")
+		for j := 0; j < len(lines); j++ {
+			counts[lines[j]]++
+		}
+	}
+
+	for key, value := range counts {
+		if value > 1 {
+			fmt.Printf("%d\t%s\n", value, key)
+		}
+	}
 }

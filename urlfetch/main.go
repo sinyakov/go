f08e03308ab5974767c9afc6455b4ea7869db901
Maxim Sinyakov <m.sinyakov@cian.ru>
2020-02-29 21:52:47 +0300

    urlfetch solution

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,25 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
+func main() {
+	argsWithoutProg := os.Args[1:]
+	for _, url := range argsWithoutProg {
+		resp, err := http.Get(url)
+		if err != nil {
+			os.Exit(1)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			os.Exit(1)
+		}
+		fmt.Printf("%s", body)
+	}
 }

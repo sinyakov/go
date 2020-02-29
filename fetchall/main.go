c01ed96c127d9f4ea6d9144af5c4596a72482f32
Maxim Sinyakov <m.sinyakov@cian.ru>
2020-03-01 02:17:36 +0300

    fetchall solution

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,41 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
 
+func main() {
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	defer resp.Body.Close() // don't leak resources
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }

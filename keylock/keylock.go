9e7547210f2fb41fead0556aae3baa1f26343847
Maxim Sinyakov <m.sinyakov@cian.ru>
2020-03-24 17:48:04 +0300

    keylock solution

--- keylock/keylock.go
+++ keylock/keylock.go
@@ -2,12 +2,87 @@
 
 package keylock
 
-type KeyLock struct{}
+import (
+	"sync"
+)
+
+type KeyLock struct {
+	lockedKeys    map[string]bool
+	mux           *sync.Mutex
+	unlockedEvent chan struct{}
+}
 
 func New() *KeyLock {
-	panic("implement me")
+	return &KeyLock{
+		lockedKeys:    make(map[string]bool),
+		mux:           &sync.Mutex{},
+		unlockedEvent: make(chan struct{}, 100000),
+	}
+}
+
+func (l *KeyLock) HasLockedKeys(keys []string) bool {
+	for _, key := range keys {
+		_, exists := l.lockedKeys[key]
+
+		if exists {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *KeyLock) lockKeys(keys []string) {
+	for _, key := range keys {
+		l.lockedKeys[key] = true
+	}
+}
+
+func (l *KeyLock) unlockKeys(keys []string) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	for _, key := range keys {
+		delete(l.lockedKeys, key)
+	}
+
+	if len(l.unlockedEvent) < 3 {
+		l.unlockedEvent <- struct{}{}
+	}
 }
 
 func (l *KeyLock) LockKeys(keys []string, cancel <-chan struct{}) (canceled bool, unlock func()) {
-	panic("implement me")
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	if !l.HasLockedKeys(keys) {
+		l.lockKeys(keys)
+
+		keysCopy := make([]string, len(keys))
+		copy(keysCopy, keys)
+
+		return false, func() {
+			l.unlockKeys(keysCopy)
+		}
+	}
+
+	for {
+		select {
+		case <-l.unlockedEvent:
+			if !l.HasLockedKeys(keys) {
+				l.lockKeys(keys)
+
+				keysCopy := make([]string, len(keys))
+				copy(keysCopy, keys)
+
+				return false, func() {
+					l.unlockKeys(keysCopy)
+				}
+			}
+		default:
+			if cancel == nil {
+				return false, func() {}
+			}
+			return true, nil
+		}
+	}
 }

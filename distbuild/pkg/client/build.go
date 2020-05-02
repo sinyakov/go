c29a4306074597c9ddb6783ef18e6fc9a45582de
Maxim Sinyakov <m.sinyakov@cian.ru>
2020-05-02 14:26:14 +0300

    remove disttest packages

--- distbuild/pkg/client/build.go
+++ distbuild/pkg/client/build.go
@@ -4,11 +4,6 @@ package client
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"io"
-
-	"gitlab.com/slon/shad-go/distbuild/pkg/api"
 
 	"go.uber.org/zap"
 
@@ -16,9 +11,6 @@ import (
 )
 
 type Client struct {
-	logger      *zap.Logger
-	apiEndpoint string
-	sourceDir   string
 }
 
 func NewClient(
@@ -26,11 +18,7 @@ func NewClient(
 	apiEndpoint string,
 	sourceDir string,
 ) *Client {
-	return &Client{
-		logger:      l,
-		apiEndpoint: apiEndpoint,
-		sourceDir:   sourceDir,
-	}
+	panic("implement me")
 }
 
 type BuildListener interface {
@@ -42,48 +30,5 @@ type BuildListener interface {
 }
 
 func (c *Client) Build(ctx context.Context, graph build.Graph, lsn BuildListener) error {
-	c.logger.Info("pkg/client/build.go Build start")
-	buildClient := api.NewBuildClient(c.logger, c.apiEndpoint)
-	_, statusReader, err := buildClient.StartBuild(ctx, &api.BuildRequest{Graph: graph})
-	if err != nil {
-		c.logger.Error("pkg/client/build.go Build", zap.Error(err))
-		return err
-	}
-	defer statusReader.Close()
-	// TODO: заливка отсутствующих файлов
-	fmt.Println("pkg/client/build.go buildStarted")
-	// spew.Dump(buildStarted)
-
-	// lsn.OnJobStdout(graph.Jobs[0].ID, []byte("OK\n"))
-	// lsn.OnJobFinished(graph.Jobs[0].ID)
-	// return nil
-
-	for {
-		// time.Sleep(time.Millisecond * 500)
-		c.logger.Info("pkg/client/build.go statusReader 1")
-		statusUpdate, err := statusReader.Next()
-		c.logger.Info("pkg/client/build.go statusReader 2")
-
-		if statusUpdate != nil && statusUpdate.JobFinished != nil {
-			c.logger.Info("pkg/client/build.go statusReader 3")
-			lsn.OnJobStdout(statusUpdate.JobFinished.ID, statusUpdate.JobFinished.Stdout) // ADDED
-			lsn.OnJobStderr(statusUpdate.JobFinished.ID, statusUpdate.JobFinished.Stderr) // ADDED
-			lsn.OnJobFinished(statusUpdate.JobFinished.ID)
-			c.logger.Info("pkg/client/build.go statusReader 4, exited")
-			return nil
-		}
-
-		// TODO: HACK
-		if errors.Is(err, io.EOF) {
-			c.logger.Info("pkg/client/build.go statusReader 5, exited")
-			return nil
-		}
-		c.logger.Info("pkg/client/build.go statusReader 6")
-
-		if err != nil {
-			c.logger.Info("pkg/client/build.go statusReader 7, exited")
-			return err
-		}
-		c.logger.Info("pkg/client/build.go statusReader 8, exited")
-	}
+	panic("implement me")
 }
